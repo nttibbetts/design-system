71e44babac4bfd6ad9de954937521c1d2ec0a833
Dane Thurber <dane.thurber@gmail.com>
2020-11-18 16:01:06 -0700

    feat(table-next): simplify filtering example

--- packages/table-next/src/react/__stories__/react-table.story.tsx
+++ packages/table-next/src/react/__stories__/react-table.story.tsx
@@ -3,17 +3,8 @@
 import * as Text from '@pluralsight/ps-design-system-text'
 import SearchInput from '@pluralsight/ps-design-system-searchinput'
 import { Meta, Story } from '@storybook/react/types-6-0'
-import { matchSorter } from 'match-sorter'
-import React, { useEffect, useMemo, useState } from 'react'
-import {
-  FilterValue,
-  IdType,
-  Row,
-  useFilters,
-  useGlobalFilter,
-  useSortBy,
-  useTable
-} from 'react-table'
+import React, { useMemo } from 'react'
+import { useGlobalFilter, useSortBy, useTable } from 'react-table'
 
 import { generateUserCourseViews } from './seed'
 import Table from '..'
@@ -21,6 +12,13 @@ import Table from '..'
 export default {
   title: 'Components/Table/react-table',
   component: Table,
+  decorators: [
+    Story => (
+      <div style={{ padding: 30 }}>
+        <Story />
+      </div>
+    )
+  ],
   parameters: {
     center: { disabled: true }
   }
@@ -97,23 +95,30 @@ export const Filtering: Story = () => {
     ],
     []
   )
-  const defaultColumn = useMemo(() => ({ Filter: ColumnFilter }), [])
 
-  const data = useMemo(() => generateUserCourseViews(), []) as any
-  const filterTypes = useMemo(() => ({ text: fuzzyTextFilter }), [])
+  const data = useMemo(() => generateUserCourseViews(100), []) as any
 
-  const plugins = [useFilters, useGlobalFilter]
-  const table = useTable(
-    { columns, data, defaultColumn, filterTypes },
-    ...plugins
-  )
+  const plugins = [useGlobalFilter]
+  const table = useTable({ columns, data }, ...plugins)
 
+  const onSearchBlur: React.FocusEventHandler<HTMLInputElement> = evt => {
+    table.setGlobalFilter(evt.target.value)
+  }
+  const onSearchChange: React.ChangeEventHandler<HTMLInputElement> = evt => {
+    table.setGlobalFilter(evt.target.value)
+  }
   return (
     <>
-      <GlobalFilter
-        globalFilter={table.state.globalFilter}
-        setGlobalFilter={table.setGlobalFilter}
-      />
+      <div style={{ display: 'flex', padding: 40 }}>
+        <Text.P style={{ marginRight: 10 }}>Search:</Text.P>
+
+        <SearchInput
+          onBlur={onSearchBlur}
+          onChange={onSearchChange}
+          style={{ flex: 1 }}
+          value={table.state.globalFilter}
+        />
+      </div>
 
       <Table {...table.getTableProps()}>
         <Table.Head>
@@ -123,7 +128,6 @@ export const Filtering: Story = () => {
                 return (
                   <Table.Header {...column.getHeaderProps()}>
                     {column.render('Header')}
-                    {column.canFilter && column.render('Filter')}
                   </Table.Header>
                 )
               })}
@@ -150,95 +154,3 @@ export const Filtering: Story = () => {
     </>
   )
 }
-
-const ColumnFilter: React.FC<{
-  column: {
-    filterValue?: string
-    setFilter: (value?: string) => void
-  }
-}> = props => {
-  const { filterValue = '', setFilter } = props.column
-  const [value, setValue] = useState(filterValue)
-
-  useEffect(() => {
-    setValue(filterValue || '')
-  }, [filterValue])
-
-  useEffect(() => {
-    setFilter(value)
-  }, [value, setFilter])
-
-  const handleBlur: React.FocusEventHandler<HTMLInputElement> = evt => {
-    setValue(evt.target.value)
-  }
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = evt => {
-    setValue(evt.target.value)
-  }
-
-  return (
-    <SearchInput onBlur={handleBlur} onChange={handleChange} value={value} />
-  )
-}
-
-const GlobalFilter: React.FC<{
-  globalFilter?: string
-  setGlobalFilter: (value?: string) => void
-}> = props => {
-  const { globalFilter = '', setGlobalFilter } = props
-  const [value, setValue] = useState(globalFilter)
-
-  useEffect(() => {
-    setValue(globalFilter || '')
-  }, [globalFilter])
-
-  useEffect(() => {
-    setGlobalFilter(value)
-  }, [value, setGlobalFilter])
-
-  const handleBlur: React.FocusEventHandler<HTMLInputElement> = evt => {
-    setValue(evt.target.value)
-  }
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = evt => {
-    setValue(evt.target.value)
-  }
-
-  return (
-    <div style={{ display: 'flex', padding: 40 }}>
-      <Text.P style={{ marginRight: 10 }}>Search:</Text.P>
-
-      <SearchInput
-        onBlur={handleBlur}
-        onChange={handleChange}
-        style={{ flex: 1 }}
-        value={value}
-      />
-    </div>
-  )
-}
-
-function fuzzyTextFilter<T extends Record<string, unknown>>(
-  rows: Array<Row<T>>,
-  id: IdType<T>,
-  filterValue: FilterValue
-) {
-  return matchSorter(rows, filterValue, {
-    keys: [(row: Row<T>) => row.values[id]]
-  })
-}
-fuzzyTextFilter.autoRemove = (val: unknown) => !val
-
-function startsWithFilter<T extends Record<string, unknown>>(
-  rows: Array<Row<T>>,
-  id: IdType<T>,
-  filterValue: FilterValue
-) {
-  return rows.filter(row => {
-    const rowValue = row.values[id]
-    return rowValue !== undefined
-      ? String(rowValue)
-          .toLowerCase()
-          .startsWith(String(filterValue).toLowerCase())
-      : true
-  })
-}
-startsWithFilter.autoRemove = (val: unknown) => !val
